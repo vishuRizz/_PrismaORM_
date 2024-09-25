1920f8cffd39aefe57532991730e8afa9824f25d
vishu pratap <vishurizz01@gmail.com>
2024-09-26 00:39:28 +0530

    expressifired the app

--- prisma_new/dist/index.js
+++ prisma_new/dist/index.js
@@ -8,56 +8,82 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-function getTodoByUserId(userId) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const res = yield prisma.todo.findMany({
-            where: {
-                userId: userId
-            },
-            select: {
-                id: true,
-                title: true,
-                description: true,
-                user: true
-            }
-        });
-        console.log(res);
-    });
-}
-getTodoByUserId(2);
-function createTodo() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const res = yield prisma.todo.create({
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("server is runing baby");
+});
+app.post("/add-user", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { userName, password, firstName, lastName, email } = req.body;
+        const response = yield prisma.user.create({
             data: {
-                title: 'complete postgreSQL',
-                description: 'complete it by tomorrow itself',
-                userId: 2
+                userName,
+                password,
+                firstName,
+                lastName,
+                email
             }
         });
-        console.log(res);
-    });
-}
-// createTodo()
-function insertUser(userName, firstName, email, lastName, password) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const res = yield prisma.user.create({
-                data: {
-                    userName,
-                    firstName,
-                    email,
-                    password,
-                    lastName
-                }
-            });
-            console.log(res);
+        res.send(response);
+    }
+    catch (error) {
+        console.log("fucking error", error);
+    }
+}));
+app.get("/get-user", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { userId } = req.body;
+        const response = yield prisma.user.findFirst({
+            select: {
+                firstName: true,
+                lastName: true,
+                userName: true
+            },
+            where: {
+                id: userId
+            }
+        });
+        res.send(response);
+    }
+    catch (error) {
+        console.log("error found", error);
+    }
+}));
+app.post("/add-todos", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId, title, description } = req.body;
+    try {
+        const response = yield prisma.todo.create({
+            data: {
+                title,
+                description,
+                userId,
+            }
+        });
+        res.send(response);
+    }
+    catch (error) {
+        console.log("fucking error found", error);
+    }
+}));
+app.get("/get-userandtodo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.body;
+    const response = yield prisma.todo.findMany({
+        where: {
+            userId
+        },
+        select: {
+            title: true,
         }
-        catch (error) {
-            console.log("fucking errror", error);
-        }
     });
-}
-// insertUser('avii', 'avi', 'avi@gmail.com', 'srivastava', 'fuckme')
+}));
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
--- prisma_new/src/index.ts
+++ prisma_new/src/index.ts
@@ -1,3 +1,80 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+import express from 'express';
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("server is runing baby")
+})
+
+app.post("/add-user",async (req, res)=>{
+    try {
+        const {userName, password, firstName, lastName, email} = req.body;
+    const response = await prisma.user.create({
+        data: {
+            userName,
+            password,
+            firstName,
+            lastName,
+            email
+        }
+    })
+    res.send(response)
+    } catch (error) {
+        console.log("fucking error", error)
+    }
+    
+})
+app.get("/get-user",async (req, res)=>{
+    try {
+        const {userId} = req.body;
+        const response = await prisma.user.findFirst({
+            select: {
+               firstName: true,
+               lastName: true,
+               userName: true 
+            },
+            where: {
+                id: userId
+            }
+        })
+        res.send(response)
+    } catch (error) {
+        console.log("error found", error);
+    }
+   
+})
+app.post("/add-todos", async (req, res)=>{
+    const {userId, title, description} = req.body;
+    try {
+        const response = await prisma.todo.create({
+            data: {
+                title,
+                description,
+                userId, 
+            }
+        })
+        res.send(response)
+    } catch (error) {
+        console.log("fucking error found", error)
+    }
+})
+app.get("/get-userandtodo", async (req, res)=>{
+    const {userId} = req.body;
+    const response = await prisma.todo.findMany({
+        where:{
+            userId
+        },
+        select:{
+            title: true,
+            
+            
+        }
+    })
+})
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000")
+})
+
